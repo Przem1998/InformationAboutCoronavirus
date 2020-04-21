9013d7c77895f9165adf188dcf489150783c01a9
Przem1998 <p.zeleznicki@protonmail.com>
2020-04-21 10:05:53 +0200

    add primary user interface with contents

---
+++ BasicsInformation.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace InformationAboutCoronavirus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BasicsInformation : Page
+    {
+        public BasicsInformation()
+        {
+            this.InitializeComponent();
+            Question1.Text = "Czym jest koronawirus?";
+            Answer1.Text = "Jest to wirus RNA osłonięty błoną tłuszczową (lipidową). " +
+                "Dzięki takiej budowie można mu zapobiegać przez zastosowanie środków chemicznych, takich jak zwykłe mydło, " +
+                "alkohol min. 60-70%, preparaty do dezynfekcji i inne wirusobójcze.";
+            Question2.Text = "Jaką chrobę wywołuje koronawirus?";
+            Answer2.Text = "Nowy koronawirus SARS - Cov - 2 wywołuje chorobę o nazwie COVID - " +
+                "19.Najczęściej występujące objawy choroby to gorączka, kaszel, duszność, problemy z oddychaniem.Chorobie mogą towarzyszyć bóle mięśni i zmęczenie ";
+            Question3.Text = "Na czym polega leczenie?";
+            Answer3.Text = "Decyzję o sposobie leczenia podejmuje lekarz. Leczenie jest wyłącznie objawowe, czyli polega na leczeniu objawów choroby, takich jak gorączka " +
+                "lub problemy z oddychaniem. W literaturze naukowej opisuje się przypadki skutecznego wyleczenia pacjentów zakażonych koronawirusem za pomocą dotychczas" +
+                " znanych leków. 13 marca 2020 r. Urząd Rejestracji Produktów Leczniczych, Wyrobów Medycznych i Produktów Biobójczych (URPL) wydał pozytywną decyzję " +
+                "w sprawie zmiany do pozwolenia na dopuszczenie do obrotu dla leku zawierającego chlorochinę, polegającą na dodaniu nowego wskazania terapeutycznego: " +
+                "„Leczenie wspomagające w zakażeniach koronawirusami typu beta takimi jak SARS-CoV, MERS-CoV i SARS-CoV-2”. Jak dotąd nie są dostępne dane dotyczące " +
+                "skuteczności chlorochiny u chorych z COVID-19 poza Chińską Republiką Ludową. Producent zabezpieczył zapas leku na potrzeby Ministerstwa Zdrowia oraz " +
+                "Agencji Rezerw Materiałowych. Zgodnie z deklaracją producenta, chlorochina będzie dystrybuowana do pacjentów z COVID-19 zgodnie z dyspozycjami Ministra Zdrowia. ";
+            Question4.Text = "Czy są jakieś specialne leki zapbiegające nowemu koronawirsowi lub leczące go?";
+            Answer4.Text = "Do tej pory nie ma konkretnego leku zalecanego do zapobiegania lub leczenia nowego koronawirusa. Osoby zakażone wirusem otrzymują leczenie objawowe" +
+                " oraz leczenie ewentualnych powikłań bakteryjnych. Jednocześnie 13 marca 2020 r. Urząd Rejestracji Produktów Leczniczych, Wyrobów Medycznych i Produktów Biobójczych (URPL)" +
+                " wydał pozytywną decyzję w sprawie zmiany do pozwolenia na dopuszczenie do obrotu dla leku zawierającego chlorochinę, polegającą na dodaniu nowego wskazania terapeutycznego:" +
+                " „Leczenie wspomagające w zakażeniach koronawirusami typu beta takimi jak SARS-CoV, MERS-CoV i SARS-CoV-2”. Jak dotąd nie są dostępne dane dotyczące skuteczności chlorochiny" +
+                " u chorych z COVID-19 poza Chińską Republiką Ludową. Producent zabezpieczył zapas leku na potrzeby Ministerstwa Zdrowia oraz Agencji Rezerw Materiałowych. Zgodnie z deklaracją" +
+                " producenta, chlorochina będzie dystrybuowana do pacjentów z COVID-19 zgodnie z dyspozycjami Ministra Zdrowia.";
+            Question5.Text = "Czy osoby starsze i młodsze są na równie podatne na koronawirusa?";
+            Answer5.Text = "Ludzie w każdym wieku mogą zostać zainfekowani przez nowy koronawirus. Osoby starsze, a także osoby z istniejącymi schorzeniami przewlekłymi (takimi jak astma, cukrzyca," +
+                " choroby serca) wydają się bardziej podatne ma wystąpienie cięższych objawów choroby. WHO zaleca osobom w każdym wieku podjęcie kroków w celu ochrony przed wirusem, np. poprzez " +
+                "przestrzeganie zasad higieny rąk i higieny dróg oddechowych.";
+            Question6.Text = "Ile się czeka na wyniki testu koronawirusa?";
+            Answer6.Text = "Samo badanie próbki trwa kilka godzin. Jeśli badanie trzeba powtórzyć, to ocena pobranego wymazu może trwać nawet kilkadziesiąt godzin. Należy doliczyć czas od pobrania " +
+                "do przewiezienia próbki do laboratorium (to zależy od tego, jak daleko jest miejsce, gdzie pobrano wymaz, od najbliższego laboratorium wykonującego badanie). Wszystko zależy zatem od logistyki.";
+        }
+
+        private void Video_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+            Video.AreTransportControlsEnabled = true;
+
+            Video.TransportControls.IsFastForwardButtonVisible = true;
+            Video.TransportControls.IsFastForwardEnabled = true;
+
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace InformationAboutCoronavirus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Strona nie załadowała się pomyślnie" + e.SourcePageType.FullName);
+        }
+
+
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("FundamentalInfomations",typeof(BasicsInformation)),
+            ("Prevention",typeof(Prevention)),
+            ("InformationForPoland", typeof(InformationForPoland)),
+            ("Plans",typeof(FuturePlans)),
+            ("InPoland", typeof(StatiscticsInPoland)),
+            ("InTheWorld", typeof(StatiscticsInTheWorld))
+        };
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigated += On_Navigated;
+            
+            NavView.SelectedItem = NavView.MenuItems[2];
+            ContentFrame.Navigate(typeof(BasicsInformation));
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                         NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked == true)
+            {
+                NavView_Navigate("settings", args.RecommendedNavigationTransitionInfo);
+            }
+            else if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+     
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+            if (navItemTag == "settings")
+            {
+               _page = typeof(SettingPage);
+            }
+            else
+            {
+                var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+                _page = item.Page;
+            }
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+            
+            if (_page != null && !Type.Equals(preNavPageType, _page))
+            {
+                ContentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+                                           NavigationViewBackRequestedEventArgs args)
+        {
+            On_BackRequested();
+        }
+
+        private bool On_BackRequested()
+        {
+            if (!ContentFrame.CanGoBack)
+                return false;
+            if (NavView.IsPaneOpen &&
+                (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+                 NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+                return false;
+
+            ContentFrame.GoBack();
+            return true;
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+          if (ContentFrame.SourcePageType == typeof(SettingPage))
+            {
+               NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+               NavView.Header = "Ustawienia";
+             }
+           else if (ContentFrame.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                NavView.Header = ((NavigationViewItem)NavView.SelectedItem).Content.ToString();
+            }
+        }
+
+      
+    }
+}
